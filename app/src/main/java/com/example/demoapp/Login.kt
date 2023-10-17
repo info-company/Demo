a7552d577eb165e69fa709f569007f6152080312
HP <info@krednote.com>
2023-10-17 13:00:42 +0530

    new

---
+++ app/src/main/java/com/example/demoapp/Login.kt
@@ -0,0 +1,67 @@
+package com.example.demoapp
+
+import android.R
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.demoapp.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class Login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth= FirebaseAuth.getInstance()
+
+
+        val Spy = binding.spinner
+        // Create an ArrayAdapter for the spinner
+        val genderOptions = arrayOf("Select Gender", "Male", "Female")
+        val adapter = ArrayAdapter(this, R.layout.simple_spinner_item, genderOptions)
+        Spy.adapter = adapter
+
+binding.textView.setOnClickListener {
+    val intent = Intent(this,Signup::class.java)
+    startActivity(intent)
+}
+        binding.button.setOnClickListener {
+            val Email = binding.email.text.toString()
+            val password =binding.pass.text.toString()
+            val spiner = binding.spinner.selectedItem.toString()
+            if(Email.isNotEmpty()&&password.isNotEmpty() && spiner.isNotEmpty()){
+
+                firebaseAuth.signInWithEmailAndPassword(Email,password).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        val selectedGender = Spy.selectedItem.toString()
+
+                        // Check the selected gender
+                        if (selectedGender == "Male") {
+                           val intent = Intent(this,Retaler::class.java)
+                            startActivity(intent)
+                        } else if (selectedGender == "Female") {
+                            val intent = Intent(this,MainActivity::class.java)
+                            startActivity(intent)
+
+                        } else {
+                           Toast.makeText(this,"You haven't choise spinner",Toast.LENGTH_LONG).show()
+                    }
+                    }else{
+                        Toast.makeText(this,it.exception.toString(),Toast.LENGTH_LONG).show()
+
+                    }
+                }
+
+            }else{
+                Toast.makeText(this,"Fields Must be fill !!",Toast.LENGTH_LONG).show()
+            }
+
+
+
+    }
+}}
---
+++ app/src/main/java/com/example/demoapp/Signup.kt
@@ -0,0 +1,48 @@
+package com.example.demoapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.demoapp.databinding.ActivityLoginBinding
+import com.example.demoapp.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class Signup : AppCompatActivity() {
+    private lateinit var binding: ActivitySignupBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth=FirebaseAuth.getInstance()
+
+        binding.submitbtn.setOnClickListener {
+            val email=binding.Email.text.toString()
+            val pass = binding.Password.text.toString()
+            val passwordcobnfr = binding.confirmpass.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && passwordcobnfr.isNotEmpty()){
+                if(pass==passwordcobnfr){
+                    firebaseAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener {
+                        if (it.isSuccessful){
+                            val intent=Intent(this,Login::class.java)
+                            startActivity(intent)
+                        }else{
+                            Toast.makeText(this,it.exception.toString(),Toast.LENGTH_LONG).show()
+
+                        }
+                    }
+                }else{
+                    Toast.makeText(this,"Password is not matching",Toast.LENGTH_LONG).show()
+
+                }
+
+            }else{
+                Toast.makeText(this,"field in unfield",Toast.LENGTH_LONG).show()
+            }
+
+        }
+    }
+}
